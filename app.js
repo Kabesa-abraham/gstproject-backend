1e480ad9f3589c2ec0629da04e1359d8c28cc304
Kabesa-abraham <yebulaabraham@gmail.com>
2025-03-06 19:06:12 +0100

    modification de quelques fichier comme app.js et .env

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ mongoose.connect(process.env.MONGO_URL).then(() => {console.log('MongoDB connect
                                        .catch((err) => console.log(err));
 
 const app = express()
-app.use(cors({origin:"http://localhost:5173",credentials:true}));
+app.use(cors({origin:process.env.FRONTEND_URL,credentials:true}));
 app.use(express.json());
 app.use(cookie_parser());
 
